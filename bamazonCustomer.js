7b549248231f2a706b63eaf08fc897bcdadf76fe
Jared <jared.harris1@g.austincc.edu>
2018-10-26 14:58:56 -0500

    .

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,15 +8,16 @@ var connection = mysql.createConnection({
     user:       'root',
     port:       '3306',
     database:   'bamazon',
-    password:   'roooooots'
+    password:   'Faster45'
 });
 //check to see if connection is successful
 connection.connect(function(err){
     if(err) throw err;
     //console.log("connection id is", connection.threadId);
-    console.log("\n" + "Welcome to (.this) Electronics Store!");
+    console.log("\n" + "Welcome to our Electronics Store!");
     ourProducts();
 });
+
 function ourProducts(){
 // instantiate
 connection.query("SELECT * FROM products", function(err, res){
@@ -29,13 +30,54 @@ connection.query("SELECT * FROM products", function(err, res){
     for(let i = 0; i < res.length; i++){
         table.push(
             [res[i].item_id,
-           res[i].product_name,
-           res[i].price,
-           res[i].stock_quantity]
+            res[i].product_name,
+            res[i].price,
+            res[i].stock_quantity]
         );
-    }
+    }//completes for loop
      
-    console.log(table.toString());
-})
-connection.end();
-}
+    console.log(table.toString() + '\n');
+    orderingPrompt();
+}) //ends connection.query();
+} //closes ourProducts();
+
+function orderingPrompt(){
+    inquirer.prompt([{
+        name: "item",
+        type: "input",
+        message: "What item ID would you like to purchase?",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log("\n" + " Please enter Item Id #:" + "\n");
+                return false;
+            }
+        }
+    },
+    {  
+        name: "amount",
+        type: "input",
+        message: "How many would you like to buy?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+        }
+    }]).then(function(userSelection){
+        //connects to db, updates table
+        var id = userSelection.item;
+        var quantity = userSelection.amount;
+    connection.query("SELECT * FROM products WHERE ?", {item_id: id}, function(err, res){
+        console.log('user Selection', userSelection);
+        console.log('Poruduct id', id);
+        console.log('quantity', quantity);
+        console.log('res', res.length);
+        //console.log('err', err);
+        
+    })
+    }); //ends .then function
+
+    connection.end();
+} //closes orderingPrompt();
